1de32eb6dc269886dc120205ae7816ded371591b
YArgueta16 <84729560+YArgueta16@users.noreply.github.com>
2021-06-09 13:32:32 -0600

    Add files via upload

---
+++ Tabla Pedido.sql
@@ -0,0 +1,9 @@
+create table pedidos(
+Id_Pedido  int primary key,
+Estado_pedido varchar(50),
+Fecha_envio datetime,
+Fecha_pedido datetime,
+Fecha_entrega datetime,
+Destinatario  varchar (50),
+Direccion varchar (50)
+)
