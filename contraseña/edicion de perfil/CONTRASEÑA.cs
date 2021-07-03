f6e1a6a60a349b5084e3bff3c812fca4f507257e
EdgardoGuzman08 <84731170+EdgardoGuzman08@users.noreply.github.com>
2021-07-02 22:33:43 -0600

    Add files via upload

---
+++ contraseña/edicion de perfil/CONTRASEÑA.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Windows.Forms;
+
+namespace edicion_de_perfil
+{
+    public partial class CONTRASEÑA : Form
+    {
+        static DirectoryEntry srvLDAPActiveDirectory;
+        public CONTRASEÑA()
+        {
+            InitializeComponent();
+        }
+
+        SqlCommand cmd;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(@"Data Source=LAPTOP-BJMBD4J6;Initial Catalog =JkBeauty;Integrated security=True"))
+            {
+                cn.Open();
+                if (textBox3.Text != textBox2.Text)
+                {
+                    MessageBox.Show("Las contraseñas no coinciden.",
+                    "Repetir contraseñas...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBox2.Focus();
+                }
+                else
+                {
+                    if (textBox2.Text == "")
+                    {
+                        MessageBox.Show("Debe indicar la nueva contraseña.",
+                        "Nueva contraseña...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox2.Focus();
+                    }
+                    else
+                    {
+                        if (textBox1.Text == "")
+                        {
+                            MessageBox.Show("la contraseña es incorrecta", "la contraseña es correcta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+
+                        Form1 ventanaForm1 = new Form1();
+                        ventanaForm1.Show();
+                        this.Hide();
+
+                        cn.Close();
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                textBox1.Select();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+                textBox2.Select();
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+                textBox3.Select();
+        }
+    }
+
+}
---
+++ contraseña/edicion de perfil/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace edicion_de_perfil
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-BJMBD4J6;Initial Catalog =JkBeauty;Integrated security=True");
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string cod = textBox2.Text;
+            string Primer_Nombre = textBox1.Text;
+            string Usuario = TXTUSUARIO.Text;
+            string Correo = TXTCORREO.Text;
+            string Contraseña = TXTCONTRASEÑA.Text;
+            string cadena = "update Empleado set Primer_Nombre='" + Primer_Nombre + "', Usuario=" + Usuario + "Correo = " + Correo + "Contraseña = " + Contraseña + " where IDEmpleado=" + cod;
+            SqlCommand cmd = new SqlCommand(cadena, con);
+
+            MessageBox.Show("Se modificaron los datos del artículo");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            TXTUSUARIO.Text = "";
+            TXTCORREO.Text = "";
+            TXTCONTRASEÑA.Text = "";
+
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            CONTRASEÑA Nuevaventana2 = new CONTRASEÑA();
+            Nuevaventana2.Show();
+            this.Hide();
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
