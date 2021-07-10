f15ef9ec289ba910285a7e56692e2b4583cd9525
EdgardoGuzman08 <84731170+EdgardoGuzman08@users.noreply.github.com>
2021-07-09 23:11:32 -0600

    Add files via upload

---
+++ busqueda/interfazBusqueda/interfazBusqueda/BusquedaPedido.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace interfazBusqueda
+{
+    public partial class BusquedaPedido : Form
+    {
+        public BusquedaPedido()
+        {
+            InitializeComponent();
+        }
+       
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-BJMBD4J6;Initial Catalog =JkBeauty;Integrated security=True");
+       
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            int Id_Pedido = 0;
+            string cod = textBox1.Text;
+            string cadena = "Select Estado_pedido, Fecha_envio, Fecha_pedido, Fecha_entrega, Destinatario, Direccion from pedido where Id_Pedido=" + cod;
+            SqlCommand cmd = new SqlCommand(cadena, con);
+            cmd.Parameters.AddWithValue("Id_Pedido", Id_Pedido);
+
+            SqlDataReader pedido = cmd.ExecuteReader();
+
+            if (pedido.Read())
+            {
+                textBox2.Text = pedido["Estado_pedido"].ToString();
+                textBox3.Text = pedido["Fecha_envio"].ToString();
+                textBox4.Text = pedido["Fecha_pedido"].ToString();
+                textBox5.Text = pedido["Fecha_entrega"].ToString();
+                textBox6.Text = pedido["Destinatario"].ToString();
+                textBox7.Text = pedido["Direccion"].ToString();
+            }
+
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Busqueda ventananueva1 = new Busqueda();
+            ventananueva1.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ busqueda/interfazBusqueda/interfazBusqueda/BusquedaProducto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+namespace interfazBusqueda
+{
+    public partial class BusquedaProducto : Form
+    {
+        public BusquedaProducto()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-BJMBD4J6;Initial Catalog =JkBeauty;Integrated security=True");
+        
+        private void aceptarbn2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            int ID_Producto = 0;
+            int Id_marca = 0;
+            string cod = textBox1.Text;
+            string cadena = "Select Id_marca, Nombre_Producto, Existencia, Precio_Actual, Fecha_Vencimiento from Producto where ID_Producto=" + cod;
+            SqlCommand cmd = new SqlCommand(cadena, con);
+            cmd.Parameters.AddWithValue("ID_Producto", ID_Producto);
+            cmd.Parameters.AddWithValue("Id_marca", Id_marca);
+
+            SqlDataReader Cliente = cmd.ExecuteReader();
+
+            if (Cliente.Read())
+            {
+                textBox6.Text = Cliente["Id_marca"].ToString();
+                textBox2.Text = Cliente["Nombre_Producto"].ToString();
+                textBox3.Text = Cliente["Existencia"].ToString();
+                textBox4.Text = Cliente["Precio_Actual"].ToString();
+                textBox5.Text = Cliente["Fecha_Vencimiento"].ToString();
+             
+            }
+
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Busqueda ventananueva1 = new Busqueda();
+            ventananueva1.Show();
+            this.Hide();
+        }
+    }
+}
