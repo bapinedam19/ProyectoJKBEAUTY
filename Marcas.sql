11b5b767cfa9c55fc300f586100905459b951633
CesarH <84744234+T350N@users.noreply.github.com>
2021-07-16 09:11:45 -0600

    Add files via upload

---
+++ Marcas.sql
@@ -0,0 +1,16 @@
+use JKBeauty;
+INSERT INTO [dbo].[Marca]
+           ([Id_marca]
+           ,[Descripcion]
+           ,[Nombre_Marca]
+           ,[cod_proveedor])
+     VALUES
+           ( '1' , 'Crema corporal', 'aVeeno', '1'),
+
+		   ('2','Gel depilador','aVeeno','1'),
+
+		   ('3','Gel de baño','aVeeno','1'),
+
+		   ('4','Exfoliante','Neutrogena','2'),
+
+		   ('5','Shampoo','Hask','1')
