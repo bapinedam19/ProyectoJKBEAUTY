ffe01156e6797af26c41a0ef605f52439604c522
YArgueta16 <84729560+YArgueta16@users.noreply.github.com>
2021-07-16 15:59:08 -0600

    Add files via upload

--- Scrip existencias.sql
+++ Scrip existencias.sql
@@ -1,8 +1,15 @@
 create table Existencias(
 Id_existencias int primary key,
 ID_Producto int,
-cantidad_existencias numeric(18),
-cantidad_bodega numeric(18),
-Cantidad_tienda numeric (18),
-Constraint fk_producto FOREIGN KEY (Id_Producto) REFERENCES Productos(ID_Producto)
+cantidad_existencias int Null,
+cantidad_bodega int Null,
+Cantidad_tienda int Null,
+Constraint fk_producto FOREIGN KEY (Id_Producto) REFERENCES Producto(ID_Producto)
 )
+
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (1, 2, 50,30,20)
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (2, 3, 60,30,30)
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (3, 4,100, 50,50)
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (4, 5,120,60,60)
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (5, 6, 40,15,25)
+INSERT [dbo].[Existencias] ([Id_existencias], [ID_Producto], [cantidad_existencias], [cantidad_bodega],[Cantidad_tienda]) VALUES (6, 7,100,50,50)
